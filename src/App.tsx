0bf6557b12accb7bf95412c0be94d00b371e3346
Diego Morais <diegosiao@gmail.com>
2021-02-04 15:55:47 -0300

    #mac-test

--- src/App.tsx
+++ src/App.tsx
@@ -9,33 +9,8 @@ const Hello = () => {
       <div className="Hello">
         <img width="200px" alt="icon" src={icon} />
       </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
+      <h1 style={{ textAlign: 'center' }}>Movi Motors</h1>
+      <h3 style={{ textAlign: 'center' }}>Sua oficina 2.0</h3>
     </div>
   );
 };
